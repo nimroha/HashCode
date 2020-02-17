174b366f8be679cdf75d93d2b2def98af3cd3692
nimroha <nimrod.morag@gmail.com>
2020-02-17 07:34:06 +0200

    2020: solved practice round

--- 2020/src/Parser.py
+++ 2020/src/Parser.py
@@ -1,19 +1,21 @@
+import numpy as np
 
 def parseIn(path):
     with open(path, 'r') as fp:
-        numPhotos = int(fp.readline().strip().split()[0])
-        photos    = []
-        for i, line in enumerate(fp):
-            orientation, numTags, *tags = line.strip().split()
-            photos.append({'orient':  orientation,
-                         'numTags': int(numTags),
-                         'tags':    tags,
-                         'id':      str(len(photos))})
+        numSlices = int(fp.readline().strip().split()[0])
+        pizzas = np.array([int(x) for x in fp.readline().strip().split()])
+        # for i, line in enumerate(fp):
+        #     orientation, numTags, *tags = line.strip().split()
+        #     photos.append({'orient':  orientation,
+        #                  'numTags': int(numTags),
+        #                  'tags':    tags,
+        #                  'id':      str(len(photos))})
 
-    return numPhotos, photos
+    return numSlices, pizzas
 
-def parseOut(path, slideshow):
-    print(slideshow)
+def parseOut(path, pizzas):
+    print(pizzas)
     with open(path, 'w') as fp:
-        fp.write('{}\n'.format(len(slideshow)))
-        [fp.write('{}\n'.format(slide)) for slide in slideshow]
+        fp.write(f'{len(pizzas)}\n')
+        [fp.write(f'{p} ') for p in pizzas]
+        fp.write('\n')
--- 2020/src/Solver.py
+++ 2020/src/Solver.py
@@ -5,42 +5,107 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from sklearn.utils import shuffle
+from time import time
 
 from src.Parser import parseIn, parseOut
 from src.Utils  import savePickle, loadPickle
 
 
-INPUTS = {'a': '../inputs/a_example',
-          'b': '../inputs/b_lovely_landscapes',
-          'c': '../inputs/c_memorable_moments',
-          'd': '../inputs/d_pet_pictures',
-          'e': '../inputs/e_shiny_selfies'}
+INPUTS = {'a': '../../inputs/a_example.in',
+          'b': '../../inputs/b_small.in',
+          'c': '../../inputs/c_medium.in',
+          'd': '../../inputs/d_quite_big.in',
+          'e': '../../inputs/e_also_big.in'}
 
 
-def main(argv=None):
+from random import seed
+from random import randint
+# seed random number generator
+seed(time())
+# generate some integers
 
-    parser = argparse.ArgumentParser(description='Solve problem')
-    parser.add_argument('--i', help='Input file id (letter)', required=True)
-    args = parser.parse_args(argv)
+def random_pizza(slices, pizzas):
+    # num_of_pizzas = len(pizzas)
+    chosen_pizzas = []
+    chosen_pizzas_idxs = []
+    used_indexes = []
+    index = randint(0, len(pizzas)-1)
+    while index not in used_indexes and \
+            np.sum(chosen_pizzas) + pizzas[index] < slices:
+        chosen_pizzas.append(pizzas[index])
+        chosen_pizzas_idxs.append(index)
+        used_indexes.append(index)
+        index = randint(0, len(pizzas)-1)
 
-    inPath  = INPUTS[args.i]
+    return np.sort(chosen_pizzas_idxs), np.sum(chosen_pizzas)
+
+
+def random_pizza_smarter(slices, pizzas):
+    # num_of_pizzas = len(pizzas)
+    chosen_pizzas = []
+    chosen_pizzas_idxs = []
+    used_indexes = []
+    while len(pizzas) > len(used_indexes):
+        index = randint(0, len(pizzas)-1)
+        if index not in used_indexes and \
+                np.sum(chosen_pizzas) + pizzas[index] < slices:
+            chosen_pizzas.append(pizzas[index])
+            chosen_pizzas_idxs.append(index)
+        used_indexes.append(index)
+
+    return np.sort(chosen_pizzas_idxs), np.sum(chosen_pizzas)
+
+
+def random_pizza_master(slices, pizzas, rounds):
+    best_num = 0
+    best = []
+    for i in range(rounds):
+        chosen_pizzas, round_sum = random_pizza_smarter(slices, pizzas)
+        if round_sum > best_num:
+            best_num = round_sum
+            best = chosen_pizzas
+
+    return best
+
+
+def solve(inputProblem):
+    inPath = os.path.abspath(os.path.join(__file__, INPUTS[inputProblem]))
     outPath = inPath + '_result.txt'
+    print(f'Solving {inPath}')
 
     print("Parsing...")
     if os.path.isfile(inPath + '.pkl'):
-        numPhotos, photos = loadPickle(inPath + '.pkl')
+        numSlices, pizzas = loadPickle(inPath + '.pkl')
     else:
-        numPhotos, photos = parseIn(inPath)
-        savePickle(inPath + '.pkl', (numPhotos, photos))
+        numSlices, pizzas = parseIn(inPath)
+        savePickle(inPath + '.pkl', (numSlices, pizzas))
 
     print("Solving...")
-    # TODO
+    selected = random_pizza_master(numSlices, pizzas, 10000)
 
     # write solution to file
     print("Writing solution to file...")
-    parseOut(outPath, None)
+    print(f'ordered {np.sum(np.take(pizzas, selected))} of {numSlices} slices')
+    parseOut(outPath, selected)
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(description='Solve problem')
+    parser.add_argument('-i', '--input', help='Input file id (letter)', default='a', choices=INPUTS.keys())
+    parser.add_argument('--loop',        help='run on all inputs',      default=False, action='store_true')
+    args = parser.parse_args(argv)
+
+    inputProblem = args.input
+
+    if args.loop:
+        for inputProblem in INPUTS.keys():
+            solve(inputProblem)
+    else:
+        solve(inputProblem)
 
     print("Done")
+
+
 
 
 if __name__ == '__main__' :
