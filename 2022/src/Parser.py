c66cec46772d1fd646a760bf504090c22fb7fe49
nimroha <nimrod.morag@gmail.com>
2022-02-24 20:43:35 +0200

    added output parser

--- 2022/src/Parser.py
+++ 2022/src/Parser.py
@@ -46,18 +46,13 @@ def parseIn(path):
         return people, projects, list(set(all_skills))
 
 
-def parseOut(path, schedules):
+def parseOut(path, plan):
     with open(path, 'w') as fp:
-        schedules = {k: v for k, v in schedules.items() if len(v) > 0}
-        fp.write(f'{len(schedules)}\n')
-        for intersection, schedule in schedules.items():
-            fp.write(f'{intersection}\n')
-            fp.write(f'{len(schedule)}\n')
-            for name, duration in schedule:
-                fp.write(f'{name} {duration}\n')
+        fp.write(f'{len(plan)}\n')
+        for project_name, members in plan:
+            fp.write(f'{project_name}\n')
+            fp.write(f'{" ".join(members)}\n')
 
 if __name__ == '__main__':
-    example_schedule = {1: [('a', 3), ('b', 2)],
-                        3: [('z', 1), ('e', 3)]}
-    parseIn('inputs/example.txt')
-    parseOut('inputs/example_result.txt', example_schedule)
+    test_plan = [('alpha', ['tom', 'brady']), ('beta', ['putin', 'biden'])]
+    parseOut('../inputs/test_result.txt', test_plan)
