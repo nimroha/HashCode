8cc960301e59b139c14003c82c7931f24d57e51a
nimroha <nimrod.morag@gmail.com>
2022-02-24 22:19:00 +0200

    handle no_skill bug

--- 2022/src/common.py
+++ 2022/src/common.py
@@ -4,7 +4,7 @@ def naive_levelup(project, members, people):
     """only levelup on your own without mentoring"""
     for member, skill_req in zip(members, project['skills']):
         skill, needed_level = skill_req
-        member_level = people[member][skill]
+        member_level = people[member].get(skill, 0)
         if needed_level == member_level:
             people[member][skill] += 1
 
@@ -13,7 +13,7 @@ def mentor_levelup(project, members, people):
     """levelup with mentor"""
     for member, skill_req in zip(members, project['skills']):
         skill, needed_level = skill_req
-        member_level = people[member][skill]
+        member_level = people[member].get(skill, 0)
         if needed_level == member_level + 1:
             mentors = [m for m in members if people[m].get(skill, 0) >= needed_level]
             if len(mentors) > 0:
