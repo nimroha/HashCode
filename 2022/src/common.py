a6324c7abddcd1eee80d991cea4f3733440631cf
nimroha <nimrod.morag@gmail.com>
2022-02-24 22:23:33 +0200

    fix it again

--- 2022/src/common.py
+++ 2022/src/common.py
@@ -15,10 +15,10 @@ def levelup(project, members, people):
         skill, needed_level = skill_req
         member_level = people[member].get(skill, 0)
         if needed_level == member_level:
-            people[member][skill] += 1
+            people[member][skill] = member_level + 1
         elif needed_level == member_level + 1:
             mentors = [m for m in members if people[m].get(skill, 0) >= needed_level]
             if len(mentors) > 0:
-                people[member][skill] += 1
+                people[member][skill] = member_level + 1
 
 
